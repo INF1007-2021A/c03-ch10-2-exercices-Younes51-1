eae2dd21f47b83cc3650bf1a26a6cccbd3187b3b
Younes51-1 <77343582+Younes51-1@users.noreply.github.com>
2021-11-22 13:30:03 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -12,10 +12,12 @@ from IPython.display import display
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+
 # TODO: Définissez vos fonctions ici
 def readfile():
-    path=os.getcwd()
-    return pd.read_csv(os.path.join(path, 'data/winequality-white.csv'), sep=';')
+    return pd.read_csv(os.path.join(os.getcwd(), 'data/winequality-white.csv'), sep=';')
 
 
 def seprate_target(df, name):
@@ -36,11 +38,33 @@ def methode_linear(X_train, y_train, X_test):
 
 
 def graphe_random (y_random, y_test):
-    plt.plot(y_random, y_test, label='predicted values')
-    plt.plot(y_test)
+    x=np.arange(0,2449,1)
+    plt.plot(x,y_test, c='cornflowerblue')    
+    plt.plot(x,y_random, c='orange')
+    plt.title('RandomForestRegressor predictions analysis')
+    plt.ylabel('Quality')
+    plt.xlabel('Number of samples')
+    plt.legend(['Target values', 'Predicted values'], loc="upper right")
+    plt.show()
+    
+    
+def graphe_linear (y_linear, y_test):
+    x=np.arange(0,2449,1)
+    plt.plot(x,y_test, c='cornflowerblue')    
+    plt.plot(x,y_linear, c='orange')
+    plt.title('LinearRegression predictions analysis')
+    plt.ylabel('Quality')
+    plt.xlabel('Number of samples')
+    plt.legend(['Target values', 'Predicted values'], loc="upper right")
     plt.show()
     
 
+def erreur_random(y_random, y_test):
+    return mean_squared_error(y_test, y_random)
+
+
+def erreur_linear(y_linear, y_test):
+    return mean_squared_error(y_test, y_linear)
 
 
 if __name__ == '__main__':
@@ -50,4 +74,8 @@ if __name__ == '__main__':
     X_train, X_test, y_train, y_test=split_df(df_filtre,quality)
     y_random=methode_random(X_train, y_train, X_test)
     y_linear=methode_linear(X_train, y_train, X_test)
-    graphe_random(y_random,y_test)
+    graphe_linear(y_linear, y_train)
+    graphe_random(y_random, y_test)
+    print(f"l'erreur random est {erreur_random(y_random, y_test)}")
+    print(f"l'erreur linear est {erreur_linear(y_linear, y_test)}")
+    
